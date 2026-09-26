cc6d039b315dc21c390444223ac12258ed51d225
Kloud-C <256132680+Kloud-C@users.noreply.github.com>
2026-09-26 16:17:39 +0900

    Use configured Kakao JavaScript key

--- assets/js/share-config.js
+++ assets/js/share-config.js
@@ -1,3 +1,3 @@
 // Add the JavaScript key from Kakao Developers > App > Platform Key.
 // This browser key is visible to site visitors; restrict its allowed domains in Kakao Developers.
-window.MOLGGA_KAKAO_JAVASCRIPT_KEY = "07e132ef362abd0a10393bb8134f7ae0";
+window.MOLGGA_KAKAO_JAVASCRIPT_KEY = "a64a5e5663d9c7508b4fb23069df9f1a";
